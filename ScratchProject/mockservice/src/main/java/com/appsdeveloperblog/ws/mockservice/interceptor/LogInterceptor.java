63af1311c123b616df61e6c518a30a7e0e4a4923
Niraj Singh <nirajsingh242@gmail.com>
2025-01-23 00:19:43 +0530

    Spring Interceptor changes in mock service

---
+++ ScratchProject/mockservice/src/main/java/com/appsdeveloperblog/ws/mockservice/interceptor/LogInterceptor.java
@@ -0,0 +1,29 @@
+package com.appsdeveloperblog.ws.mockservice.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LogInterceptor implements HandlerInterceptor {
+    private final Logger logger = LoggerFactory.getLogger(LogInterceptor.class);
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.info("after completion -request URL : " + handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.info("postHandle -request URL : " + response.getStatus() + " " + response);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("prehandle -request URL : " + request.getRemoteUser() + " " + request.getRequestURI());
+        return false;
+    }
+
+}
